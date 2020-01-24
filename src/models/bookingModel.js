a12b11b130fb7af4156d655646f489ca1370a10d
tenor1 <tenor@ukr.net>
2020-01-24 20:15:09 +0200

    add comment to model

--- src/models/bookingModel.js
+++ src/models/bookingModel.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var bookingSchema = new Schema({
-    room: Number,
-    from: Number,
-    to: Number
+    room: Number,  // room number
+    from: Number,  // start booking date
+    to: Number     // end booking date
 })
 
 module.exports = bookingSchema;
